de132c0ef07f9d505186e0516d65dab32a7c9c40
Milana <milan.ffhhbbjut@gmail.com>
2024-08-31 19:01:17 +0300

    new commit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import classes from './Dialogs.module.css'
 const Dialogs = () => {
     return (
         <div className={classes.content}>
-            Dialogs
+            Dialogs привет
         </div>
     )
 }
